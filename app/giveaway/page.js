19e733974d662ae0a1aafdefaae51453fce35cbc
parisintzes <parisintzes@gmail.com>
2026-06-12 20:24:08 +0300

    Create page.js
    Create OMMT route structure

---
+++ app/giveaway/page.js
@@ -0,0 +1,15 @@
+export default function Page() {
+
+  return (
+
+    <main style={{ padding: 40, color: "white", background: "#02050c", minHeight: "100vh" }}>
+
+      <h1>OMMT Airlines</h1>
+
+      <p>Environment under construction.</p>
+
+    </main>
+
+  );
+
+}
